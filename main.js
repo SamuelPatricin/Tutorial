5316e1f055369e4582e42b5c5ed5b221bd52097a
SamuelPatricin <166259994+SamuelPatricin@users.noreply.github.com>
2024-11-25 17:31:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function MyButton() {
+    return (
+      <button>
+        Sou um botao
+      </button>
+    );
+  }
+  
+  export default function MyApp() {
+    return (
+      <div>
+        <h1>Welcome to my app</h1>
+        <MyButton />
+      </div>
+    );
+  }
